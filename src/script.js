90d93e0d76018268899789fd3d4d15b8c6c892ef
SuperWebMonkey <vikingsattack@outlook.com>
2024-10-07 13:54:54 -0700

    initial commit

---
+++ src/script.js
@@ -0,0 +1,11 @@
+import { central, db1, db2, db3, vault } from "./db.js";
+
+function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
+}
+
+console.log("hello world");
